bfbf2fca9fb6f581cf14981b3e83e852ed72813a
Kévin Le Calvez <kevin.lecalvez3@gmail.com>
2018-11-16 11:35:47 +0100

    Ajout nova score

--- main.py
+++ main.py
@@ -18,12 +18,28 @@ def webcrawler(page):
         print('Nom prodtuit : ' + name)
 
         lien_product = product.find("a").get("href")
-        print('Lien produits : ' + home + lien_product)
+        lien = home + lien_product
+        print('Lien produits : ' + lien)
 
         lien_image = product.find("img")
         print('Lien image : ',  lien_image['data-src'])
 
+        getDetailProduit(lien)
+
         print('####################')
+
+
+def getDetailProduit(url):
+    r = requests.get(url)
+    c = r.content
+    soup = BeautifulSoup(c, 'html.parser')
+    #div = soup.find('div', {"class": "medium-12 large-8 xlarge-8 xxlarge-8 columns"})
+    nova = soup.findAll('a', {"href": "/nova"})
+
+    for img in nova:
+        imgNova = img.find('img')
+        if imgNova is not None:
+            print('Nova score : ' + imgNova['alt'])
 
 
 webcrawler(home)
