03cd6a3170c4979f07995160fb853dfd9c2689a7
andre <andreasadelson@gmail.com>
2018-11-16 11:30:56 +0100

    Scroll d'une page pour les infos des produits

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ home = 'https://fr.openfoodfacts.org/'
 def webcrawler(page):
     r = requests.get(page)
     c = r.content
-
+    data = []
     soup = BeautifulSoup(c, 'html.parser')
 
     product_list = soup.find('ul', class_="products")
     for product in product_list.findAll("li"):
         name = product.find("span").text
-        print('Nom prodtuit : ' + name)
-
         lien_product = product.find("a").get("href")
-        print('Lien produits : ' + home + lien_product)
-
         lien_image = product.find("img")
-        print('Lien image : ',  lien_image['data-src'])
-
-        print('####################')
+        data.append({
+            "name": name,
+            "url_product": lien_product,
+            "url_image": lien_image['data-src']
+        })
+    print(data)
+    print(len(data))
 
 
 webcrawler(home)
