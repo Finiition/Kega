d842be3c7e5a3056bed0090fae63cb3a95e60c2d
Esteban Le Calvez <esteban.le-calvez@kurmi-software.com>
2018-11-26 11:23:34 +0100

    Interface graphique basique
    interface graphique basique

--- main.py
+++ main.py
@@ -2,58 +2,103 @@
 
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask,render_template
 
 home = 'https://fr.openfoodfacts.org/'
+
+app = Flask(__name__)
+produits = []
+details = []
 
 
 def webcrawler(page):
     r = requests.get(page)
     c = r.content
-    data = []
     soup = BeautifulSoup(c, 'html.parser')
-
+    global produits;
     product_list = soup.find('ul', class_="products")
     for product in product_list.findAll("li"):
         name = product.find("span").text
         lien_product = product.find("a").get("href")
-        lien_image = product.find("img")
-        data.append({
-            "name": name,
-            "url_product": lien_product,
-            "url_image": lien_image['data-src']
+        lien = home + lien_product
+        lien_image = getProductInfos(lien)
+        nova = getDetailProduit(lien)
+        if("4" in str(nova)):
+            color = "red"
+        if("3" in str(nova)):
+            color = "orange"
+        if("2" in str(nova)):
+            color = "yellow"
+        if("1" in str(nova)):
+            color = "green"
+        i = 0
+        produits.append({
+            "name":name,
+            "url_product":lien_product,
+            "lien_image":lien_image,
+            "nova": nova,
+            "color": color,
+            #"nutri_img": details[i].imgNutriscore,
+            #"nutriscoreText": details[i].nutriscoreText,
         })
-    print(data)
-    print(len(data))
-    return data
+        i=i+1
+
+def getProductInfos(url):
+    r = requests.get(url)
+    c = r.content
+    global details;
+    soup = BeautifulSoup(c, 'html.parser')
+    nova = soup.findAll('a', {"href": "/nova"})
+    nutriscore = soup.findAll('a', {"href": "/nutriscore"})
+    imgProduct = soup.find('img',{"id":"og_image"})["data-src"]
+
+    for img in nova:
+        imgNova = img.find('img')
+        if imgNova is not None:
+            finalNova = imgNova['alt'];
+
+    for img in nutriscore:
+        imgNutriscore = img.find('img')
+        if imgNutriscore is not None:
+            final_nutriscore_img = imgNutriscore["src"];
+            final_nutriscore_alt = imgNutriscore["alt"];
 
 
-req = 'https://fr.openfoodfacts.org/produit/3045320104172/100-pur-jus-clementines-pressees-andros'
+            #print("Nova ALT : "+str(finalNova)+ "///// nutriscore SRC : " +
+             #     str(final_nutriscore_img) + "///// nutriscore ALT :"
+              #    + str(final_nutriscore_alt)+ " ///// img product : "
+               #   +str(imgProduct));
+
+
+            details.append({
+                "novaText": finalNova,
+                "imgProduct": imgProduct,
+                'imgNutriscore': final_nutriscore_img,
+                "nutriscoreText": final_nutriscore_alt
+            })
 
 
 def getDetailProduit(url):
     r = requests.get(url)
     c = r.content
     soup = BeautifulSoup(c, 'html.parser')
-    # div = soup.find('div', {"class": "medium-12 large-8 xlarge-8 xxlarge-8 columns"})
     nova = soup.findAll('a', {"href": "/nova"})
-    novaScore = 0
-    # div = soup.find('div', {"class": "medium-12 large-8 xlarge-8 xxlarge-8 columns"})
-    nova = soup.findAll('a', {"href": "/nova"})
+    img = soup.findAll('img')
 
     for img in nova:
         imgNova = img.find('img')
         if imgNova is not None:
-            if imgNova['alt'] == '1 - Aliments non transformés ou transformés minimalement':
-                novaScore = 4
-            if imgNova['alt'] == '2 - Ingrédients culinaires transformés':
-                novaScore = 3
-            if imgNova['alt'] == '3 - Aliments transformés':
-                novaScore = 2
-            if imgNova['alt'] == '4 -  Produits alimentaires et boissons  ultra-tran':
-                novaScore = 1
-
-    print(novaScore)
+            return imgNova['alt'];
 
 
-getDetailProduit(req)
+
 webcrawler(home)
+
+
+@app.route("/")
+def main():
+    return render_template('produits.html', produits=produits)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=4000)
