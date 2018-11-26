6ba82485037cdc3ed7bd27adddfa8413afaf2360
AdelsonAndreas <andreas.adelson@epitech.eu>
2018-11-26 15:45:49 +0100

    Pagination ok

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 home = 'https://fr.openfoodfacts.org/'
 
+numpage = 1
 app = Flask(__name__)
 produits = []
 details = []
@@ -15,12 +16,15 @@ def webcrawler(page):
     r = requests.get(page)
     c = r.content
     soup = BeautifulSoup(c, 'html.parser')
-    global produits;
+    nutri_number = 0
+    nova_number = 0
+    global numpage
+    global produits
+    global details
     bio = soup.find('a', {"href": "/label/bio"})
-    #Liste des produits
+    # Liste des produits
     product_list = soup.find('ul', class_="products")
-    i = 0
-    #Création de l'objet contenant les informations du produit.
+    # Création de l'objet contenant les informations du produit.
 
     if bio is None:
         bio_number = 0
@@ -32,48 +36,54 @@ def webcrawler(page):
         lien_product = product.find("a").get("href")
         lien = home + lien_product
         getProductInfos(lien)
-        lien_image = details[i]["imgProduct"]
-        nutriscoreText =details[i]["nutriscoreText"]
-        nova = details[i]["novaText"]
-        if("4" in str(nova)):
-            nova_number = 1
-        elif("3" in str(nova)):
-            nova_number = 2
-        elif("2" in str(nova)):
-            nova_number = 3
-        elif("1" in str(nova)):
-            nova_number = 4
-        if("A" in str(nutriscoreText)):
-            nutri_number = 5
-        elif("B" in str(nutriscoreText)):
-            nutri_number = 4
-        elif("C" in str(nutriscoreText)):
-            nutri_number = 3
-        elif("D" in str(nutriscoreText)):
-            nutri_number = 2
-        elif("E" in str(nutriscoreText)):
-            nutri_number = 1
+        for data in details:
+            lien_image = data["imgProduct"]
+            nutriscoreText = data["nutriscoreText"]
+            nova = data["novaText"]
+        if ("4" in str(nova)):
+            nova_number = 1 / 4
+        elif ("3" in str(nova)):
+            nova_number = 2 / 4
+        elif ("2" in str(nova)):
+            nova_number = 3 / 4
+        elif ("1" in str(nova)):
+            nova_number = 4 / 4
+        if ("A" in str(nutriscoreText)):
+            nutri_number = 5 / 5
+        elif ("B" in str(nutriscoreText)):
+            nutri_number = 4 / 5
+        elif ("C" in str(nutriscoreText)):
+            nutri_number = 3 / 5
+        elif ("D" in str(nutriscoreText)):
+            nutri_number = 2 / 5
+        elif ("E" in str(nutriscoreText)):
+            nutri_number = 1 / 5
 
-        score = getScore(nova_number, nutri_number, bio_number)
+        score = int(float(getScore(nova_number, nutri_number, bio_number)))
+        if water(lien):
+            score = 100
 
         if score < 26:
             color = 'red'
-        elif score >= 26 & score < 51:
+        elif score >= 26 and score < 51:
             color = 'orange'
-        elif score >= 51 & score <76:
+        elif score >= 51 and score < 76:
             color = 'yellow'
         elif score > 75:
-            color = 'green'
+            color = '#b6e945'
         produits.append({
-            "name":name,
-            "url_product":lien_product,
-            "lien_image":lien_image,
+            "name": name,
+            "url_product": lien_product,
+            "lien_image": lien_image,
             "nova": nova,
             "score": score,
             "color": color,
             "nutriscoreText": nutriscoreText,
         })
-        i = i+1
+    numpage = numpage + 1
+
+
+
 
 def getProductInfos(url):
     r = requests.get(url)
@@ -82,7 +92,7 @@ def getProductInfos(url):
     soup = BeautifulSoup(c, 'html.parser')
     nova = soup.findAll('a', {"href": "/nova"})
     nutriscore = soup.findAll('a', {"href": "/nutriscore"})
-    imgProduct = soup.find('img',{"id":"og_image"})["data-src"]
+    imgProduct = soup.find('img', {"id": "og_image"})["data-src"]
     for img in nova:
         imgNova = img.find('img')
         if imgNova is not None:
@@ -92,15 +102,18 @@ def getProductInfos(url):
         imgNutriscore = img.find('img')
         if imgNutriscore is not None:
             final_nutriscore_alt = imgNutriscore["alt"];
+        else:
+            final_nutriscore_alt = "4"
 
-            details.append({
-                "novaText": finalNova,
-                "imgProduct": imgProduct,
-                "nutriscoreText": final_nutriscore_alt
-            })
+        details.append({
+            "novaText": finalNova,
+            "imgProduct": imgProduct,
+            "nutriscoreText": final_nutriscore_alt
+        })
+
 
 def getScore(novascore, nutriscore, bioscore):
-    score = ((0.6 * nutriscore) + (0.3 * novascore) + (0.1 * bioscore))
+    score = ((0.6 * nutriscore) + (0.3 * novascore) + (0.1 * bioscore)) * 100
     return score
 
 
@@ -117,12 +130,11 @@ def water(url):  # retourne True si l'élément est de l'eau, False sinon
     return eau
 
 
-if water("https://fr.openfoodfacts.org/produit/3274080005003/eau-de-source-cristaline"): #exemple utilisation water()
-    score = 100
-    print("C'est de l'eau !")
-
-webcrawler(home)
-
+while numpage < 10:
+    print(home + str(numpage))
+    webcrawler(home + str(numpage))
+#print("produits",produits)
+#print("details", details)
 
 @app.route("/")
 def main():
@@ -131,4 +143,3 @@ def main():
 
 if __name__ == '__main__':
     app.run(host='localhost', port=4000)
-
